6580d1956ef5b775891c0f4c592d7b705ba879de
goofyseeker311 <goofyseeker311@gmail.com>
2025-02-01 12:01:14 +0200

    Update BinaryNeuralNetwork.java
    Added Network and Node data structure classes.

--- src/fi/jkauppa/javabinaryneuralnetwork/BinaryNeuralNetwork.java
+++ src/fi/jkauppa/javabinaryneuralnetwork/BinaryNeuralNetwork.java
@@ -1,5 +1,7 @@
 package fi.jkauppa.javabinaryneuralnetwork;
 
+import java.util.ArrayList;
+
 public class BinaryNeuralNetwork {
 
 	public BinaryNeuralNetwork() {
@@ -12,17 +14,22 @@ public class BinaryNeuralNetwork {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("BinaryNeuralNetwork v0.0.2");
 		BinaryNeuralNetwork app = new BinaryNeuralNetwork();
 		app.run();
 		System.out.println("exit.");
 	}
 	
-	private class Network {
-		
+	private static class Network {
+		ArrayList<Node> nodelist = new ArrayList<Node>();
 	}
 	
-	private class Node {
-		
+	private static class Node {
+		public int data = 0;
+		public int mask = 0;
+		public int weight = 0;
+		public Node out = null;
+		public int outbit = 0;
 	}
 
 }
