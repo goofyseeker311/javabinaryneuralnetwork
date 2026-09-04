747d7cc428e888674aed6c68663be861ab948311
goofyseeker311 <goofyseeker311@gmail.com>
2026-09-04 22:45:14 +0300

    Update tam.m
    changed to fp16 quantization. added halftofloat() and floattohalf() fp16 float conversion functions.

--- tam.m
+++ tam.m
@@ -34,6 +34,40 @@ function minivalue = floattomini(floatvalue)
   bitvalue = bitunpack(floatvalue);
   minivalue = bitpack(bitvalue(:,1:8),'int8');
 endfunction
+function floatvalue = halftofloat(halfvalue)
+  longvalue = cast(halfvalue,'int64');
+  longsign = bitshift(bitand(longvalue,cast(0x8000,'int64')),-15);
+  longexp = bitshift(bitand(longvalue,cast(0x7C00,'int64')),-10) - 15 + 127;
+  longfrac = bitand(longvalue,cast(0x3FF,'int64'));
+  if (longexp == 112) longexp = cast(0x0,'int64');
+  elseif (longexp == 143) longexp = cast(0xFF,'int64'); endif
+  longfloat = bitor(bitor( (bitshift(longsign,31)) , (bitshift(longexp,23)) ) , (bitshift(longfrac,13)) );
+  bitfloat = bitunpack(longfloat);
+  floatvalue = bitpack(bitfloat(:,1:32),'single');
+endfunction
+function halfvalue = floattohalf(floatvalue)
+  longvalue = cast(typecast(cast(floatvalue,'single'),'int32'),'int64');
+  longsign = bitshift(bitand(longvalue,cast(0x80000000,'int64')),-31);
+  longexp = bitshift(bitand(longvalue,cast(0x7F800000,'int64')),-23) - 127 + 15;
+  longfrac = bitshift(bitand(longvalue,cast(0x7FFFFF,'int64')),-13);
+  if (longexp == -112) longexp = cast(0,'int64');
+  elseif (longexp == 143) longexp = cast(0x1F,'int64');
+  elseif (longexp <= 0) longexp = cast(0,'int64'); longfrac = cast(0,'int64');
+  elseif (longexp >= 31) longexp = cast(0x1F,'int64'); longfrac = cast(0,'int64');
+  else
+    roundup = bitand(longvalue,cast(0x1000,'int64'));
+    if (roundup!=0)
+      if (longfrac==0x3FF)
+        longexp += 1;
+      endif
+      longfrac += 1;
+      longfrac = bitand(longfrac,cast(0x3FF,'int64'));
+    endif
+  endif
+  floatvalue = bitor(bitor( (bitshift(longsign,15)) , (bitshift(longexp,10))) , longfrac);
+  bitvalue = bitunpack(floatvalue);
+  halfvalue = bitpack(bitvalue(:,1:16),'int16');
+endfunction
 
 [img,fs] = audioread("audio.mp3");
 
@@ -66,12 +100,12 @@ vinv = (eye(swordslen)/vv')';
 printf("svdinv (%i,%i).\n",size(vv,2),size(vv,1));
 
 bb = vinv * swordscentered';
-sc = 256 / max(abs([min(bb(:)) max(bb(:))]));
+sc = 65536 / max(abs([min(bb(:)) max(bb(:))]));
 bb *= sc;
-bbq = zeros(size(bb),'uint8');
+bbq = zeros(size(bb),'uint16');
 for n = 1:size(bb,1)
   for m = 1:size(bb,2)
-    bbq(n,m) = floattomini(bb(n,m));
+    bbq(n,m) = floattohalf(bb(n,m));
   endfor
 endfor
 bb = bbq;
@@ -82,7 +116,7 @@ load audio.mat;
 bbq = zeros(size(bb),'single');
 for n = 1:size(bb,1)
   for m = 1:size(bb,2)
-    bbq(n,m) = minitofloat(bb(n,m));
+    bbq(n,m) = halftofloat(bb(n,m));
   endfor
 endfor
 bb = cast(bbq,'double') / sc;
