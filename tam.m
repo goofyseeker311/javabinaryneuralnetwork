9d5b1ade20f83e4ba974dc45631040c577b74b8a
goofyseeker311 <goofyseeker311@gmail.com>
2026-09-04 21:38:13 +0300

    Update tam.m
    changed to fp8 quantization that results in very small compressed file size, but has audio quality issues. filtering might be required. added minitofloat() and floattomini() fp8 float conversion functions that are very slow.

--- tam.m
+++ tam.m
@@ -1,5 +1,40 @@
 close all; clear; output_precision(16);
 
+function floatvalue = minitofloat(minivalue)
+  longvalue = cast(minivalue,'int64');
+  longsign = bitshift(bitand(longvalue,cast(0x80,'int64')),-7);
+  longexp = bitshift(bitand(longvalue,cast(0x78,'int64')),-3) - 7 + 127;
+  longfrac = bitand(longvalue,cast(0x7,'int64'));
+  if (longexp == 120) longexp = cast(0x0,'int64');
+  elseif (longexp == 135) longexp = cast(0xFF,'int64'); endif
+  longfloat = bitor(bitor( (bitshift(longsign,31)) , (bitshift(longexp,23)) ) , (bitshift(longfrac,20)) );
+  bitfloat = bitunpack(longfloat);
+  floatvalue = bitpack(bitfloat(:,1:32),'single');
+endfunction
+function minivalue = floattomini(floatvalue)
+  longvalue = cast(typecast(cast(floatvalue,'single'),'int32'),'int64');
+  longsign = bitshift(bitand(longvalue,cast(0x80000000,'int64')),-31);
+  longexp = bitshift(bitand(longvalue,cast(0x7F800000,'int64')),-23) - 127 + 7;
+  longfrac = bitshift(bitand(longvalue,cast(0x7FFFFF,'int64')),-20);
+  if (longexp == -120) longexp = cast(0,'int64');
+  elseif (longexp == 135) longexp = cast(0xF,'int64');
+  elseif (longexp <= 0) longexp = cast(0,'int64'); longfrac = cast(0,'int64');
+  elseif (longexp >= 15) longexp = cast(0xF,'int64'); longfrac = cast(0,'int64');
+  else
+    roundup = bitand(longvalue,cast(0x80000,'int64'));
+    if (roundup!=0)
+      if (longfrac==0x7)
+        longexp += 1;
+      endif
+      longfrac += 1;
+      longfrac = bitand(longfrac,cast(0x7,'int64'));
+    endif
+  endif
+  floatvalue = bitor(bitor( (bitshift(longsign,7)) , (bitshift(longexp,3))) , longfrac);
+  bitvalue = bitunpack(floatvalue);
+  minivalue = bitpack(bitvalue(:,1:8),'int8');
+endfunction
+
 [img,fs] = audioread("audio.mp3");
 
 tiledim = 128;
@@ -31,14 +66,26 @@ vinv = (eye(swordslen)/vv')';
 printf("svdinv (%i,%i).\n",size(vv,2),size(vv,1));
 
 bb = vinv * swordscentered';
-sc = 128 / max(abs([min(bb(:)) max(bb(:))]));
-bb = cast(bb * sc, 'single');
-bb(abs(bb(:))<0.5) = 0;
+sc = 256 / max(abs([min(bb(:)) max(bb(:))]));
+bb *= sc;
+bbq = zeros(size(bb),'uint8');
+for n = 1:size(bb,1)
+  for m = 1:size(bb,2)
+    bbq(n,m) = floattomini(bb(n,m));
+  endfor
+endfor
+bb = bbq;
 save -binary -zip audio.mat bb sc;
 
 clear bb sc;
 load audio.mat;
-bb = cast(bb, 'double') / sc;
+bbq = zeros(size(bb),'single');
+for n = 1:size(bb,1)
+  for m = 1:size(bb,2)
+    bbq(n,m) = minitofloat(bb(n,m));
+  endfor
+endfor
+bb = cast(bbq,'double') / sc;
 
 aa = (vv * bb)' + swordsmean;
 cc = svdcomps / swordslen;
