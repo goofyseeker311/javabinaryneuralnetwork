e91ee67560e4ddd15a0da7db709bb6876696354d
goofyseeker311 <goofyseeker311@gmail.com>
2026-09-05 23:51:40 +0300

    Update tpm.m
    changed to tpm.13-int8 quantization that has highest quality compression. compressed file size is 1.1mib tpm vs 7.4mib jpeg of similar quality.

--- tpm.m
+++ tpm.m
@@ -1,74 +1,5 @@
 close all; clear; output_precision(16);
 
-function floatvalue = minitofloat(minivalue)
-  longvalue = cast(minivalue,'int64');
-  longsign = bitshift(bitand(longvalue,cast(0x80,'int64')),-7);
-  longexp = bitshift(bitand(longvalue,cast(0x78,'int64')),-3) - 7 + 127;
-  longfrac = bitand(longvalue,cast(0x7,'int64'));
-  if (longexp == 120) longexp = cast(0x0,'int64');
-  elseif (longexp == 135) longexp = cast(0xFF,'int64'); endif
-  longfloat = bitor(bitor( (bitshift(longsign,31)) , (bitshift(longexp,23)) ) , (bitshift(longfrac,20)) );
-  bitfloat = bitunpack(longfloat);
-  floatvalue = bitpack(bitfloat(:,1:32),'single');
-endfunction
-function minivalue = floattomini(floatvalue)
-  longvalue = cast(typecast(cast(floatvalue,'single'),'int32'),'int64');
-  longsign = bitshift(bitand(longvalue,cast(0x80000000,'int64')),-31);
-  longexp = bitshift(bitand(longvalue,cast(0x7F800000,'int64')),-23) - 127 + 7;
-  longfrac = bitshift(bitand(longvalue,cast(0x7FFFFF,'int64')),-20);
-  if (longexp == -120) longexp = cast(0,'int64');
-  elseif (longexp == 135) longexp = cast(0xF,'int64');
-  elseif (longexp <= 0) longexp = cast(0,'int64'); longfrac = cast(0,'int64');
-  elseif (longexp >= 15) longexp = cast(0xF,'int64'); longfrac = cast(0,'int64');
-  else
-    roundup = bitand(longvalue,cast(0x80000,'int64'));
-    if (roundup!=0)
-      if (longfrac==0x7)
-        longexp += 1;
-      endif
-      longfrac += 1;
-      longfrac = bitand(longfrac,cast(0x7,'int64'));
-    endif
-  endif
-  floatvalue = bitor(bitor( (bitshift(longsign,7)) , (bitshift(longexp,3))) , longfrac);
-  bitvalue = bitunpack(floatvalue);
-  minivalue = bitpack(bitvalue(:,1:8),'int8');
-endfunction
-function floatvalue = halftofloat(halfvalue)
-  longvalue = cast(halfvalue,'int64');
-  longsign = bitshift(bitand(longvalue,cast(0x8000,'int64')),-15);
-  longexp = bitshift(bitand(longvalue,cast(0x7C00,'int64')),-10) - 15 + 127;
-  longfrac = bitand(longvalue,cast(0x3FF,'int64'));
-  if (longexp == 112) longexp = cast(0x0,'int64');
-  elseif (longexp == 143) longexp = cast(0xFF,'int64'); endif
-  longfloat = bitor(bitor( (bitshift(longsign,31)) , (bitshift(longexp,23)) ) , (bitshift(longfrac,13)) );
-  bitfloat = bitunpack(longfloat);
-  floatvalue = bitpack(bitfloat(:,1:32),'single');
-endfunction
-function halfvalue = floattohalf(floatvalue)
-  longvalue = cast(typecast(cast(floatvalue,'single'),'int32'),'int64');
-  longsign = bitshift(bitand(longvalue,cast(0x80000000,'int64')),-31);
-  longexp = bitshift(bitand(longvalue,cast(0x7F800000,'int64')),-23) - 127 + 15;
-  longfrac = bitshift(bitand(longvalue,cast(0x7FFFFF,'int64')),-13);
-  if (longexp == -112) longexp = cast(0,'int64');
-  elseif (longexp == 143) longexp = cast(0x1F,'int64');
-  elseif (longexp <= 0) longexp = cast(0,'int64'); longfrac = cast(0,'int64');
-  elseif (longexp >= 31) longexp = cast(0x1F,'int64'); longfrac = cast(0,'int64');
-  else
-    roundup = bitand(longvalue,cast(0x1000,'int64'));
-    if (roundup!=0)
-      if (longfrac==0x3FF)
-        longexp += 1;
-      endif
-      longfrac += 1;
-      longfrac = bitand(longfrac,cast(0x3FF,'int64'));
-    endif
-  endif
-  floatvalue = bitor(bitor( (bitshift(longsign,15)) , (bitshift(longexp,10))) , longfrac);
-  bitvalue = bitunpack(floatvalue);
-  halfvalue = bitpack(bitvalue(:,1:16),'int16');
-endfunction
-
 img = imread("image.jpg");
 
 tiledim = 16;
@@ -103,26 +34,13 @@ vinv = (eye(swordslen)/vv')';
 printf("svdinv (%i,%i).\n",size(vv,2),size(vv,1));
 
 bb = vinv * swordscentered';
-sc = 240 / max(abs([min(bb(:)) max(bb(:))]));
-bb *= sc;
-bbq = zeros(size(bb),'int8');
-for n = 1:size(bb,1)
-  for m = 1:size(bb,2)
-    bbq(n,m) = floattomini(bb(n,m));
-  endfor
-endfor
-bb = bbq;
+sc = 128 / max(abs([min(bb(:)) max(bb(:))]));
+bb = cast(bb * sc,'int8');
 save -binary -zip image.mat bb sc;
 
 clear bb sc;
 load image.mat;
-bbq = zeros(size(bb),'single');
-for n = 1:size(bb,1)
-  for m = 1:size(bb,2)
-    bbq(n,m) = minitofloat(bb(n,m));
-  endfor
-endfor
-bb = cast(bbq,'double') / sc;
+bb = cast(bb,'double') / sc;
 
 aa = (vv * bb)' + swordsmean;
 cc = svdcomps / swordslen;
