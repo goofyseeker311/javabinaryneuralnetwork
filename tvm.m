a5a50d7dfc5c668b900f7957da0cc8aef1daf408
goofyseeker311 <goofyseeker311@gmail.com>
2026-09-10 02:52:50 +0300

    Update tvm.m
    changed to 32 frame chunk based compression with delta frame and tpm.13 svd components compression testing.

--- tvm.m
+++ tvm.m
@@ -3,10 +3,10 @@ close all; clear; output_precision(16);
 pkg load video;
 
 load tpm.mat;
+svdcomps = 100;
+vinv = vinv(1:svdcomps,:);
 swordslen = size(vinv,1);
-svdcomps = swordslen;
-v = eye(swordslen)/vinv;
-vv = v(:,1:svdcomps);
+vv = (eye(swordslen)/vinv')';
 
 vid = VideoReader("video.mp4");
 vframes = vid.NumberOfFrames;
@@ -20,30 +20,59 @@ tilex = ceil(imgx/tiledim);
 tiley = ceil(imgy/tiledim);
 tilesmp = tilex*tiley;
 
+cframes = 32;
+chunkorigins = 1:cframes;
+chunkscales = ones(1,cframes);
+chunkmeans = zeros(tilergb,cframes);
+chunkdata = ones(svdcomps,tilesmp,cframes,'int8');
+
 mkdir output;
-data = zeros(tilesmp,tilergb);
+words = zeros(tilesmp,tilergb,cframes);
 
-for fn = 1:vframes
-  img = vid.readFrame;
 
-  img(tiley*tiledim,tilex*tiledim,:) = [0,0,0];
-  for n = 1:tiley
-    for m = 1:tilex
-      tile = img((n-1)*tiledim+(1:16),(m-1)*tiledim+(1:16),:);
-      data((n-1)*tilex+m,:) = reshape(tile,1,tilergb);
+for fc = 1:cframes:vframes
+
+  for k = 1:cframes
+    img = vid.readFrame;
+    img(tiley*tiledim,tilex*tiledim,:) = [0,0,0];
+    for n = 1:tiley
+      for m = 1:tilex
+        tile = img((n-1)*tiledim+(1:16),(m-1)*tiledim+(1:16),:);
+        words((n-1)*tilex+m,:,k) = reshape(tile,1,tilergb);
+      endfor
     endfor
   endfor
 
-  swordsfull = cast(data,"double");
-  swordsmean = mean(swordsfull,1);
-  swordscentered = swordsfull - swordsmean;
+  chunkfull = cast(words,"double");
+  chunkmeans = mean(chunkfull,1);
+  chunkcentered = chunkfull - chunkmeans;
 
-  bb = vinv * swordscentered';
-  sc = 128 / max(abs([min(bb(:)) max(bb(:))]));
-  bb = cast(bb * sc,'int8');
+  for fn = 1:cframes
+    bb = vinv * chunkcentered(:,:,fn)';
+    sc = 128 / max(abs([min(bb(:)) max(bb(:))]));
+    if (isinf(sc)) sc = 1; endif
+    bb = cast(bb * sc,'int8');
+    bbz = sum(bb(:)==0);
+    corigin = fn;
+    if (fn>1)
+      bb2 = vinv * (chunkcentered(:,:,fn)-chunkcentered(:,:,fn-1))';
+      sc2 = 128 / max(abs([min(bb2(:)) max(bb2(:))]));
+      if (isinf(sc2)) sc2 = 1; endif
+      bb2 = cast(bb2 * sc2,'int8');
+      bbz2 = sum(bb2(:)==0);
+      if (bbz2>bbz)
+        bb = bb2;
+        sc = sc2;
+        corigin = fn - 1;
+      endif
+    endif
+    chunkorigins(1,fn) = corigin;
+    chunkscales(1,fn) = sc;
+    chunkdata(:,:,fn) = bb;
+  endfor
 
-  savefile = sprintf("output/video%i.mat",fn);
-  save("-binary", "-zip", savefile, "bb", "sc", "swordsmean", "swordslen", "svdcomps", "tilex", "tiley", "tiledim", "imgx", "imgy");
+  savefile = sprintf("output/video%i.mat",fc);
+  save("-binary", "-zip", savefile, "chunkdata", "chunkorigins", "chunkscales", "chunkmeans", "swordslen", "svdcomps", "tilex", "tiley", "tiledim", "imgx", "imgy");
 endfor
 
 #save -binary -zip video.mat store sc swordsmean swordslen svdcomps tilex tiley tiledim imgx imgy;
