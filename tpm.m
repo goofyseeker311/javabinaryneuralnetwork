1b78cb5fb77e154a390c4f105a660fa7cd26e6f4
goofyseeker311 <goofyseeker311@gmail.com>
2026-09-14 06:27:25 +0300

    Update tpm.m
    changed to custom fp8-int8 quantization.

--- tpm.m
+++ tpm.m
@@ -1,5 +1,15 @@
 close all; clear; output_precision(16);
 
+function intval = fptoint8(fpval)
+  intval = log(abs(fpval))*13.19035+64;
+  intval(intval<0) = 0;
+  intval = intval.*sign(fpval);
+endfunction
+
+function fpval = int8tofp(intval)
+  fpval = sign(intval).*exp((abs(intval)-64)/13.19035);
+endfunction
+
 img = imread("image.jpg");
 
 tiledim = 16;
@@ -38,15 +48,19 @@ printf("svdinv (%i,%i).\n",size(vv,2),size(vv,1));
 
 bb = vinv * swordscentered';
 sc = 128 / max(abs([min(bb(:)) max(bb(:))]));
-bb = cast(bb * sc,'int8');
-sd = 128 / max(abs([min(vv(:)) max(vv(:))]));
-vv = cast(vv * sd,'int8');
-save -binary -zip image.mat bb sc vv sd swordsmean swordslen svdcomps svdstep tilex tiley tiledim imgx imgy;
+if (isinf(sc)) sc = 1; endif
+bb = cast(fptoint8(bb * sc),'int8');
+#sd = 128 / max(abs([min(vv(:)) max(vv(:))]));
+#if (isinf(sd)) sd = 1; endif
+#vv = cast(fptoint8(vv * sd),'int8');
+#save -binary -zip image.mat bb sc vv sd swordsmean swordslen svdcomps svdstep tilex tiley tiledim imgx imgy;
+save -binary -zip image.mat bb sc swordsmean swordslen svdcomps svdstep tilex tiley tiledim imgx imgy;
 
-clear bb sc vv sd;
+clear bb sc;
+#clear vv sd;
 load image.mat;
-bb = cast(bb,'double') / sc;
-vv = cast(vv,'double') / sd;
+bb = int8tofp(cast(bb,'double')) / sc;
+#vv = int8tofp(cast(vv,'double')) / sd;
 
 aa = (vv * bb)' + swordsmean;
 cc = svdcomps / swordslen;
@@ -66,7 +80,7 @@ img2 = cast(img2, "uint8");
 img = img(1:imgy,1:imgx,:);
 img2 = img2(1:imgy,1:imgx,:);
 
-figure(1); image(img);
-figure(2); image(img2);
+figure(1); image(img); daspect([1 1]); set (gca, "Position", [0 0 1 1]); axis off;
+figure(2); image(img2); daspect([1 1]); set (gca, "Position", [0 0 1 1]); axis off;
 printf("compression ratio: %i/%i=%f, average/std error: %f+%f\n",svdcomps,swordslen,cc,dd,dds);
 
